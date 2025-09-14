57d243a2b1bbae6a447771a763ead3f6ff07cdba
kavya383 <skkavya501@gmail.com>
2025-09-14 17:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ vec_path = os.path.join(base_dir, "vectorizer.pkl")
 
 model = pickle.load(open(model_path, "rb"))
 vectorizer = pickle.load(open(vec_path, "rb"))
-@app.route('/')
+@app.route('/', methods=['GET','POST])
 def home():
     return "Flask server is running!"
-    
-@app.route('/predict', methods=['POST'])
-def predict():
+def index():
+    if request.method == 'GET':
+        return "Fake News Detector API is running! Send POST request with JSON {'news':'text'}"
     data = request.get_json()
     text = data.get('news', '')
+    if not text:
+        return jsonify({"error":"No news text provided"}),400
     vec = vectorizer.transform([text])
     prediction = model.predict(vec)[0]
     return jsonify({"result": "FAKE" if int(prediction) == 1 else "REAL"})
 
 if __name__ == '__main__':
-    print("Starting local Flask server on localhost...")
     app.run(debug=True)
